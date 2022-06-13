de3743b81e2c4005ed91332fa8f8362dd4a9dd88
iggoren <entaev@mail.ru>
2022-06-13 13:11:14 +0800

    commit1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc.dao;
 
+import jm.task.core.jdbc.util.Util;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +13,124 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS users (Id int not null primary key auto_increment," +
+                " name VARCHAR(64), lastName VARCHAR(64),age INT)";
+        final Connection connection = Util.getInstance().getConnection();
+        try (Statement statement = connection.createStatement()) {
+            // создание таблицы
+            statement.executeUpdate(sql);
+            System.out.println("Таблица создана!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
     }
 
     public void dropUsersTable() {
+        final Connection connection = Util.getInstance().getConnection();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO users(name,lastName,age) VALUES( ?, ?, ?)";
+        final Connection connection = Util.getInstance().getConnection();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
     }
 
     public void removeUserById(long id) {
-
+        final Connection connection = Util.getInstance().getConnection();
+        try (Statement statement = connection.createStatement()) {
+            String sql = "DELETE FROM users where id";
+            statement.executeUpdate(sql);
+            System.out.println("User " + id + " удален");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        List<User> result = new ArrayList<>();
+        final Connection connection = Util.getInstance().getConnection();
+        try (Statement statement = connection.createStatement()) {
+
+            ResultSet rs = statement.executeQuery("select * from users");
+            while (rs.next()) {
+                User users = new User();
+                users.setId((long) rs.getInt("id"));
+                users.setName(rs.getString("name"));
+                users.setLastName(rs.getString("lastName"));
+                users.setAge(rs.getByte("age"));
+                result.add(users);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
+        return result;
     }
 
     public void cleanUsersTable() {
-
+        final Connection connection = Util.getInstance().getConnection();
+        try (Statement statement = connection.createStatement()) {
+            String sql = "DELETE FROM users";
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mySQL://localhost:3306/test1";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private Util() {
+    }
+
+    private static volatile Util instance;
+    private static Connection connection = null;
+
+    public static Util getInstance() {
+        Util localInstance = instance;
+        if (localInstance == null) {
+            synchronized (Util.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new Util();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+    public static Connection getConnection() {
+        try {
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("соединение установлено");
+            }
+        } catch (
+                SQLException e) {
+            System.out.println("ошибка соединения");
+        }
+        return connection;
+    }
 }
